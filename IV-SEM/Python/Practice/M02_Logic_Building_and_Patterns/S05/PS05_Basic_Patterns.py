bc8afd58254f40a0132eb99cf53bbb21e943489a
sarayu-kandhukuri <kandhukurisarayu@gmail.com>
2026-03-13 11:42:10 +0530

    M02_Assignments_Completed

--- IV-SEM/Python/Practice/M02_Logic_Building_and_Patterns/S05/PS05_Basic_Patterns.py
+++ IV-SEM/Python/Practice/M02_Logic_Building_and_Patterns/S05/PS05_Basic_Patterns.py
@@ -1,5 +1,21 @@
-rows = 4
+#rows = 4
 
-for i in range(rows):
-    print(" " * (rows - i - 1), end="")
-    print("*" * (2* i + 1))
+#for i in range(rows):
+    #print(" " * (rows - i - 1), end="")
+    #print("*" * (2* i + 1))
+
+#n=int(input())
+#for i in range(1,n+1):
+    #print(" "*(n-i)+"* "*i)  
+#for i in range(n-1,0,-1):
+    #print(" "*(n-i)+"* "*i)
+n=Int(input())
+for i in range(1,n+1):
+
+
+
+
+
+
+
+
